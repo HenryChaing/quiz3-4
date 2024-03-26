36ac28c91eeebc5af127fa4163f85aae20d8b865
HenryChaing <n96121210@gs.ncku.edu.tw>
2024-03-26 19:42:59 +0800

    Implement a linear time complexity Hamming distance
    The time complexity of the Hamming distance will only be affected by
    the array size. Additionally, I've switched from camelCase to
    snake_case naming convention.

--- hamming_distance.c
+++ hamming_distance.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int totalHammingDistance(int* nums, int numsSize)
+int total_hamming_distance(int* nums, int numsSize)
 {
     int total = 0;;
     for (int i = 0;i < numsSize;i++)
@@ -10,8 +10,29 @@ int totalHammingDistance(int* nums, int numsSize)
     return total >> 1;
 }
 
+int hamming_distance_linear(int* nums, int numsSize){
+    int iteration = 0, distance = 0;
+    for (__uint32_t i = 1; iteration < 32; i<<=1)
+    {
+        int ones = 0;
+        for (int j = 0; j < numsSize; j++)
+        {
+            __uint32_t bin = nums[j];
+            if((bin & i) == i){
+                ones++;
+            }
+        }
+
+        distance += ones*(numsSize-ones);
+
+        iteration++;
+    }
+    
+    return distance;
+}
+
 int main(){
-    int arr[3] = {4,4,14};
-    printf("%d\n",totalHammingDistance(arr,sizeof(arr)/sizeof(arr[0])));
+    int arr[3] = {2,4,14};
+    printf("%d\n",hamming_distance_linear(arr,sizeof(arr)/sizeof(arr[0])));
 }
 
